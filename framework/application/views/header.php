454c8a1b9d03ffae63991ec8789e659e5606edac
Kione <rwgmez@gmail.com>
2015-08-08 14:50:07 -0700

    Changed the robots meta to allow link following

--- framework/application/views/header.php
+++ framework/application/views/header.php
@@ -16,7 +16,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php echo $description; ?>">
-        <meta name="robots" content="index, nofollow">
+        <meta name="robots" content="index, follow">
 
         <title><?php echo $title; ?></title>
         
