dd10f3900378733e75a63b51796af15d993dde28
Rich Gomez <0xkione@gmail.com>
2017-04-26 15:59:43 -0600

    Adding main page and static controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title per page
+  def full_title(page_title = '')
+    base_title = "Rich Gomez"
+    if page_title.empty?
+      base_title
+    else
+      page_title + '|' + base_title
+    end
+  end
 end
